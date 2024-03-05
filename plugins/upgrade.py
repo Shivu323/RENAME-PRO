f3f9112219d727a2476cc58564fbaa2663d9fab3
Shivu323 <139479837+Shivu323@users.noreply.github.com>
2024-03-05 16:03:56 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -9,25 +9,25 @@ async def upgrade(bot,update):
 	
 	**🪙 Basic** 
 	Daily  Upload  limit 20GB
-	Price Rs 49  ind /🌎 0.59$  per Month
+	Price Rs 49  ind
 	
 	**⚡ Standard**
 	Daily Upload limit 50GB
-	Price Rs 99  ind /🌎 1.19$  per Month
+	Price Rs 99  ind
 	
 	**💎 Pro**
 	Daily Upload limit 100GB
-	Price Rs 179  ind /🌎 2.16$  per Month
+	Price Rs 179  ind
 	
 	
-	Pay Using Upi I'd `hxbots@sbi`
+	Pay Using Upi I'd `s-hegad@paytm`
 	
 	After Payment Send Screenshots Of 
-        Payment To Admin @Kirodewal"""
+        Payment To Admin @Shreeshiva323"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/kirodewal")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://upayme.vercel.app/Hxbots@sbi"),
-        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://upayme.vercel.app/Hxbots@sbi")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("Admin",url = "https://t.me/Shreeshiva323")], 
+        			[InlineKeyboardButton("Phone Pay",url = "s-hegad@paytm"),
+        			InlineKeyboardButton("Paytm Wallet/UPI",url = "s-hegad@paytm")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
@@ -39,25 +39,25 @@ async def upgradecm(bot,message):
 	
 	**🪙 Basic** 
 	Daily  Upload  limit 20GB
-	Price Rs 49  ind /🌎 0.59$  per Month
+	Price Rs 49  ind 
 	
 	**⚡ Standard**
 	Daily Upload limit 50GB
-	Price Rs 99  ind /🌎 1.19$  per Month
+	Price Rs 99  ind 
 	
 	**💎 Pro**
 	Daily Upload limit 100GB
-	Price Rs 179  ind /🌎 2.16$  per Month
+	Price Rs 179  ind
 	
 	
-	Pay Using Upi I'd `HxBots@sbi`
+	Pay Using Upi I'd `s-hegad@paytm`
 	
 	After Payment Send Screenshots Of 
-        Payment To Admin @Kirodewal"""
+        Payment To Admin @Shreeshiva323"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/kirodewal")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://upayme.vercel.app/Hxbots@sbi"),
-        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://upayme.vercel.app/Hxbots@sbi")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("Admin",url = "https://t.me/Shreeshiva323")], 
+        			[InlineKeyboardButton("Phone Pay",url = "s-hegad@paytm"),
+        			InlineKeyboardButton("Paytm Wallet/UPI",url = "s-hegad@paytm")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
 
 
