86fe4eb281a909d742769840e2cc3fbe04711289
Shivu323 <139479837+Shivu323@users.noreply.github.com>
2024-03-05 17:34:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ OWNER = int(os.environ.get("OWNER", ""))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001997350110"))
 
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
